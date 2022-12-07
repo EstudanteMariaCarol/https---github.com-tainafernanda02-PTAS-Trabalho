7c38dd72f1164416b40a78b064f3aba916aabb8b
MariaCarol <86033398+EstudanteMariaCarol@users.noreply.github.com>
2022-12-07 15:56:54 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,69 @@
+//banco de dados
+let url ="https://amused-vestments-calf.cyclic.app";
+const main = document.querySelector('main');
+
+window.addEventListener('load', e => {
+    postNews(); 
+    "use strict";//restrito a funcionar em navegadores comES6 >
+    if("serviceWorker" in navigator){
+        navigator.serviceWorker.register("./sw.js");
+    }
+}); 
+
+async function postNews() {
+    const res = await fetch(url);
+    const data = await res.json();
+    main.innerHTML = data.map(createArticle).join('\n');
+}
+
+function createArticle(article){
+    return `
+           <article class="card" style="width: 18rem;" >
+     <img src="${article.imagem}" class="card-img-top" alt="..." />
+           <div class="card-body">
+             <h5 class="card-title">${article.nome}</h5>
+             <p class="card-text"> R$ ${article.preco}</p>
+             <p class="card-text">  ${article.descricao}</p>
+             <a href="#" class="btn btn-primary">Comprar</a>
+           </div>
+         </div>    
+           </article>
+    `
+}
+let posicaoInicial;//variavel para capturar a posicao
+const capturarLocalizacao = document.getElementById('localizacao');
+const latitude = document.getElementById('latitude');
+const longitude = document.getElementById('longitude');
+const map = document.getElementById('mapa');
+
+const sucesso = (posicao) => {//callback de sucesso para captura da posicao
+    posicaoInicial = posicao;
+    latitude.innerHTML = posicaoInicial.coords.latitude;
+    longitude.innerHTML = posicaoInicial.coords.longitude;
+};
+
+const erro = (error) => {//callback de error (falha para captura de localizacao)
+    let errorMessage;
+    switch(error.code){
+        case 0:
+            errorMessage = "Erro desconhecido"
+        break;
+        case 1:
+            errorMessage = "Permissão negada!"
+        break;
+        case 2:
+            errorMessage = "Captura de posição indisponível!"
+        break;
+        case 3:
+            errorMessage = "Tempo de solicitação excedido!" 
+        break;
+    }
+    console.log('Ocorreu um erro: ' + errorMessage);
+};
+
+capturarLocalizacao.addEventListener('click', () => {
+    navigator.geolocation.getCurrentPosition(sucesso, erro);
+
+    map.src = "https://maps.google.com/maps?q="+ posicaoInicial.coords.latitude+"," + posicaoInicial.coords.longitude +"&z=16&output=embed"
+
+});
